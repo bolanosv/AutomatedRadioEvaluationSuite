e178150eeb1e0bd67f3c26ec6177512ded7fbd3e
Jose Abraham Bolanos Vargas <bolanosv@purdue.edu>
2024-10-11 12:49:24 -0400

    Added identical vs non-identical antenna code logic.

--- src/main/measureAntennaGain.m
+++ src/main/measureAntennaGain.m
@@ -1,8 +1,10 @@
 function antennaGain_dBi = measureAntennaGain(Frequency, sParameter_dB, Spacing, GainFile)
     % This function calculates the gain of a test antenna in decibels relative 
     % to an isotropic radiator (dBi) based on the input frequency, S-parameters, 
-    % and spacing between the antennas. It assumes that both the test antennas 
-    % are identical and uses the Friis transmission equation for the calculation.
+    % and spacing between the antennas. If both antennas are identical the
+    % function uses the two antenna method (Friis Transmission Equation),
+    % else the DUT antenna gain is caluclated using the provided reference
+    % antenna gain.
     %
     % Parameters
     % Frequency:     A scalar or vector of frequency values (in Hz) at
@@ -11,10 +13,25 @@ function antennaGain_dBi = measureAntennaGain(Frequency, sParameter_dB, Spacing,
     %                power transfer between two antennas.
     % Spacing:       The distance (in meters) between the two antennas used
     %                in the test setup.
+    % GainFile:      (Optional) Path to a CSV or Excel file containing 
+    %                boresight gain data.
     %
     % Returns
     % antennaGain_dBi: Numerical vector containing the antenna gain (dBi)
     %                  of the test antenna, over all specified frequencies.
+
+    if nargin < 4
+        GainFile = [];
+    elseif ~isempty(GainFile)
+        [~, ~, ext] = fileparts(GainFile);
+        if ~ismember(lower(ext), {'.csv', '.xls', '.xlsx'})
+            error('GainFile must be a CSV or Excel file.');
+        end
+    end
+
+    if length(Frequency) ~= length(sParameter_dB)
+        error('Frequency and S-Parameter (dB) must have the same length.');
+    end
 
     % Speed of light (m/s)  
     c = 3E8;  
@@ -28,11 +45,10 @@ function antennaGain_dBi = measureAntennaGain(Frequency, sParameter_dB, Spacing,
     % Calculate the complex exponential factor
     friis_factor = (lambda ./ (4*pi*Spacing)).^2;
 
-    % Calculate gain in linear 
-    if ~isempty(GainFile)
+    if ~isempty(GainFile) % Non-identical Antennas
         referenceGain = loadData(GainFile);
         antenna_gain = sParameter^2 ./ (referenceGain .* friis_factor);
-    else
+    else % Identical Antennas
         antenna_gain = sqrt(sParameter ./ friis_factor);  
     end
 
