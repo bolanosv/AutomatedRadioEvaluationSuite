17f554cc4a11ea3fef52a678166482d61995a62a
Jose Abraham Bolanos Vargas <bolanosv@purdue.edu>
2024-10-14 00:10:40 -0400

    Finsihed sweepRotation function. Resolved plotting error with app properties. Added fix for scan resources problem leaving only 1 address, but have not testes this.

--- untitled2.m
+++ untitled2.m
@@ -4,7 +4,9 @@ VNAFreqs = data.FrequencyHz(1:601)';
 s21 = data.S21(1:601)';
 setupSpacing = 0.838;
 
-BoresightGain_dBi = measureAntennaGain(VNAFreqs, s21, setupSpacing);   
+
+
+BoresightGain_dBi = measureAntennaGain(VNAFreqs, s21, setupSpacing, );   
 
 combinedData = [double(VNAFreqs/1E9)', double(BoresightGain_dBi)'];
 combinedNames = {'Frequency (GHz)', 'Boresight Gain (dBi)'};
