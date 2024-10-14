a17c0a7d44f5ced507896cc3346e529946e1de9f
Jose Abraham Bolanos Vargas <bolanosv@purdue.edu>
2024-10-13 20:08:51 -0400

    Testing

---
+++ untitled2.m
@@ -0,0 +1,15 @@
+
+data = loadData('Antenna');
+VNAFreqs = data.FrequencyHz(1:601)';
+s21 = data.S21(1:601)';
+setupSpacing = 0.838;
+
+BoresightGain_dBi = measureAntennaGain(VNAFreqs, s21, setupSpacing);   
+
+combinedData = [double(VNAFreqs/1E9)', double(BoresightGain_dBi)'];
+combinedNames = {'Frequency (GHz)', 'Boresight Gain (dBi)'};
+
+numRows = size(combinedData, 1);
+disp(['Number of rows in combinedData: ', num2str(numRows)]);
+
+saveData(combinedData, combinedNames);
