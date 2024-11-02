e82dc27fb261ae4fb9e51014de41fae9196eaf44
Jose Abraham Bolanos Vargas <bolanosv@purdue.edu>
2024-11-01 20:22:50 -0400

    Tested Antenna

--- src/support/saveData.m
+++ src/support/saveData.m
@@ -1,20 +1,32 @@
 function saveData(combinedData, combinedNames)
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
     % This function saves data from the application into either a CSV or
     % Excel file. The user passes in the combined test data and combined 
     % test variable names, the function saves and organizes the data.
     %
-    % Parameters
+    % PARAMETERS
     % combinedData:  Cell array containing the data for all measurement
     %                variables. Example: {testFreq, testGain, ...}
     % combinedNames: Cell array containing the titles of the measurement
     %                variables. Example: {'Frequency Hz', 'Gain dB', ...}
+    % passedLimit:   
+    %
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
     dataTable = array2table(combinedData, 'VariableNames', combinedNames);
 
-    % Prompt the user to save the data into either CSV or Excel file
+    % if passedLimit
+    %     % Prompt the user to save the data into a CSV file
+    %     [filename, path] = uiputfile({'*.csv', 'CSV Files (*.csv)';}, 'Save Data As');
+    % else
+    %     % Prompt the user to save the data into either CSV or Excel file
+    %     [filename, path] = uiputfile({'*.csv', 'CSV Files (*.csv)'; ...
+    %                                    '*.xlsx', 'Excel Files (*.xlsx)'}, ...
+    %                                    'Save Data As');
+    % end
     [filename, path] = uiputfile({'*.csv', 'CSV Files (*.csv)'; ...
-                                   '*.xlsx', 'Excel Files (*.xlsx)'}, ...
-                                   'Save Data As');
+                                       '*.xlsx', 'Excel Files (*.xlsx)'}, ...
+                                       'Save Data As');
 
     % Hadle the user cancelling the prompt
     if isequal(filename, 0) || isequal(path, 0)
