9f4002b1521bf22eda447aa79f7d7ff844de918c
Alex Santiago <davisitosantiago11@gmail.com>
2025-03-28 16:16:56 -0400

    Deembedding Function
    Created function to generate input and output attenuation for small and large signal responses from fixed attenuations, S parameter measurements, and driver measurement.

---
+++ src/support/PAFunctions/deembedPA.m
@@ -0,0 +1,72 @@
+function [inCal,outCal] = deembedPA(mode, freq, RFInputPower, inFixedAtt, outFixedAtt, inSpFile, outSpFile, driverFile)
+    %deembedPA: De-embed PA measurement
+    %   Outputs: inCal (input attenuation), outCal (output attenuation)
+    %   + Add the inCal factor [dB] to the input RF power to obtain the correted PA input power.
+    %   - Substract outCal factor from the measured output power to get the PA output power.
+    %   
+    %   Inputs: mode (calibration mode), freq (measurement frequency [Hz]),
+    %   RFInputPower (RF input power [dBm]),  {inFixedAtt, outFixedAtt}
+    %   (fixed attenuations [dB]), {inSpFile, outSpFile} (filenames for
+    %   S parameters), driverFile (filename for deembedded driver measurement).
+    %   
+    %   Removes the effects of passive and active devices to deembed the
+    %   measurement and generates calibration factors in dB to correct the
+    %   PA input and output RF power.
+    
+    if strcmp(mode, "None")
+        % Calibration factors are 0
+
+        inCal = 0;
+        outCal = 0;
+    elseif strcmp(mode, "Fixed Attenuation")
+        % Calibration factors are the fixed attenuation in dB
+
+        inCal = inFixedAtt;
+        outCal = outFixedAtt;
+    elseif strcmp(mode, "Small Signal")
+        % Calibration factors add the fixed attenuation and the
+        % interpolated S parameters from the given filename
+        
+        % Load S parameters for input and output deembeding
+        SnP_in = sparameters(inSpFile);
+        SnP_out = sparameters(outSpFile);
+        
+        % Interpolate the attenuation at the measurement frequency
+        Att_in = -interp1(SnP_in.Frequencies,A2dB(squeeze(abs(SnP_in.Parameters(2,1,:)))),freq,'spline');
+        Att_out = -interp1(SnP_out.Frequencies,A2dB(squeeze(abs(SnP_out.SnP_in.Parameters(2,1,:)))),freq,'spline');
+
+        % Add the attenuations
+        inCal = inFixedAtt + Att_in;
+        outCal = outFixedAtt + Att_out;
+    elseif strcmp(mode, "Small + Large Signal")
+        % Adds the fixed attenuation, S parameters, and driver response
+
+        % Interpolate the attenuation at the measurement frequency
+        Att_in = -interp1(SnP_in.Frequencies,A2dB(squeeze(abs(SnP_in.Parameters(2,1,:)))),freq,'spline');
+        Att_out = -interp1(SnP_out.Frequencies,A2dB(squeeze(abs(SnP_out.SnP_in.Parameters(2,1,:)))),freq,'spline');
+
+        % Add the attenuations
+        inCal = inFixedAtt + Att_in;
+        outCal = outFixedAtt + Att_out;
+
+        % Get the driver output power for the current input power
+        driver = readtable(driverFile);
+
+        % Get measured driver frequencies and input powers
+        driverFreq = unique(driver.Frequency)*1E6;
+        driverPower = unique(driver.RFInputPower);
+        
+        % Interpolate the measurement frequency with driver data
+        driverFreq = interp1(driverFreq,driverFreq,freq,'nearest');
+
+        % Interpolate 
+        driverPower = interp1(driverPower,driverPower,RFInputPower-inCal,'nearest');
+
+        % Obtain the driver large signal gain for given measurement
+        driverGain = driver(driver.Frequency==driverFreq && driver.RFInputPower==driverPower);
+        driverGain = table2array(driverGain.Gain); driverGain = driverGain(1); % Convert to number
+        
+        % Recalculate inCal to include driver gain
+        inCal = inFixedAtt + Att_in - driverGain;
+    end
+end
