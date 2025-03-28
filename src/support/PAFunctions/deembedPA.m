ab63f4b128580e659058fe60b534f43ce38f55c8
bolanosv <bolanosv@purdue.edu>
2025-03-28 19:14:51 -0400

    Added App Support to deembedPA function

--- src/support/PAFunctions/deembedPA.m
+++ src/support/PAFunctions/deembedPA.m
@@ -1,5 +1,5 @@
-function [inCal,outCal] = deembedPA(mode, freq, RFInputPower, inFixedAtt, outFixedAtt, inSpFile, outSpFile, driverFile)
-    %deembedPA: De-embed PA measurement
+function [inCal,outCal] = deembedPA(app, freq, RFInputPower)
+    %   deembedPA: De-embed PA measurement
     %   Outputs: inCal (input attenuation), outCal (output attenuation)
     %   + Add the inCal factor [dB] to the input RF power to obtain the correted PA input power.
     %   - Substract outCal factor from the measured output power to get the PA output power.
@@ -13,44 +13,48 @@ function [inCal,outCal] = deembedPA(mode, freq, RFInputPower, inFixedAtt, outFix
     %   measurement and generates calibration factors in dB to correct the
     %   PA input and output RF power.
     
-    if strcmp(mode, "None")
+    if strcmp(app.CalibrationMode, "None")
         % Calibration factors are 0
 
         inCal = 0;
         outCal = 0;
-    elseif strcmp(mode, "Fixed Attenuation")
+    elseif strcmp(app.CalibrationMode, "Fixed Attenuation")
         % Calibration factors are the fixed attenuation in dB
 
-        inCal = inFixedAtt;
-        outCal = outFixedAtt;
-    elseif strcmp(mode, "Small Signal")
+        inCal = app.InputAttenuationValueField.Value;
+        outCal = app.OutputAttenuationValueField.Value;
+    elseif strcmp(app.CalibrationMode, "Small Signal")
         % Calibration factors add the fixed attenuation and the
         % interpolated S parameters from the given filename
         
         % Load S parameters for input and output deembeding
-        SnP_in = sparameters(inSpFile);
-        SnP_out = sparameters(outSpFile);
+        SnP_in = sparameters(app.InputSpFile);
+        SnP_out = sparameters(app.OutputSpFile);
         
         % Interpolate the attenuation at the measurement frequency
         Att_in = -interp1(SnP_in.Frequencies,A2dB(squeeze(abs(SnP_in.Parameters(2,1,:)))),freq,'spline');
         Att_out = -interp1(SnP_out.Frequencies,A2dB(squeeze(abs(SnP_out.SnP_in.Parameters(2,1,:)))),freq,'spline');
 
         % Add the attenuations
-        inCal = inFixedAtt + Att_in;
-        outCal = outFixedAtt + Att_out;
-    elseif strcmp(mode, "Small + Large Signal")
+        inCal = app.InputAttenuationValueField.Value + Att_in;
+        outCal = app.OutputAttenuationValueField.Value + Att_out;
+    elseif strcmp(app.CalibrationMode, "Small + Large Signal")
         % Adds the fixed attenuation, S parameters, and driver response
+
+        % Load S parameters for input and output deembeding
+        SnP_in = sparameters(app.InputSpFile);
+        SnP_out = sparameters(app.OutputSpFile);
 
         % Interpolate the attenuation at the measurement frequency
         Att_in = -interp1(SnP_in.Frequencies,A2dB(squeeze(abs(SnP_in.Parameters(2,1,:)))),freq,'spline');
-        Att_out = -interp1(SnP_out.Frequencies,A2dB(squeeze(abs(SnP_out.SnP_in.Parameters(2,1,:)))),freq,'spline');
+        Att_out = -interp1(SnP_out.Frequencies,A2dB(squeeze(abs(SnP_out.Parameters(2,1,:)))),freq,'spline');
 
         % Add the attenuations
-        inCal = inFixedAtt + Att_in;
-        outCal = outFixedAtt + Att_out;
+        inCal = app.InputAttenuationValueField.Value + Att_in;
+        outCal = app.OutputAttenuationValueField.Value + Att_out;
 
         % Get the driver output power for the current input power
-        driver = readtable(driverFile);
+        driver = readtable(app.DriverFile);
 
         % Get measured driver frequencies and input powers
         driverFreq = unique(driver.Frequency)*1E6;
@@ -67,6 +71,6 @@ function [inCal,outCal] = deembedPA(mode, freq, RFInputPower, inFixedAtt, outFix
         driverGain = table2array(driverGain.Gain); driverGain = driverGain(1); % Convert to number
         
         % Recalculate inCal to include driver gain
-        inCal = inFixedAtt + Att_in - driverGain;
+        inCal = app.InputAttenuationValueField.Value + Att_in - driverGain;
     end
 end
